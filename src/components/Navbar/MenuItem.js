239bd21098ed12a9d79997c41ff7f3103c7711b7
Jesica <13519011@std.stei.itb.ac.id>
2022-06-10 12:23:15 +0700

    initial commit

---
+++ src/components/Navbar/MenuItem.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Categories',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Register',
+        url: '#',
+        cName: 'nav-links'
+    },
+    // {
+    //     title: 'Sign up',
+    //     url: '#',
+    //     cName: 'nav-links-mobile'
+    // },
+]
